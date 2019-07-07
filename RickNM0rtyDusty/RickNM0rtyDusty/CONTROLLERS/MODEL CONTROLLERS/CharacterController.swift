4d1b433e703b5a8ac4bff0b90f4399602eead5fe
Austin West <korruptedanomaly@gmail.com>
2019-07-07 15:26:17 -0600

    Character controller finished

--- RickNM0rtyDusty/RickNM0rtyDusty/CONTROLLERS/MODEL CONTROLLERS/CharacterController.swift
+++ RickNM0rtyDusty/RickNM0rtyDusty/CONTROLLERS/MODEL CONTROLLERS/CharacterController.swift
@@ -10,4 +10,47 @@ import Foundation
 
 class CharacterController {
     
+    //MARK: - (optional) Singleton/Source of Truth
+    static let shared = CharacterController()
+    private init() {}
+    
+    //MARK: - Properties
+    var characterList: [Character] = []
+    let baseURLString = "https://rickandmortyapi.com/api/"
+    let characterString = "character"
+    
+    // URL I WANT TO MAKE
+    // https://rickandmortyapi.com/api/character/
+    
+    //MARK: - File functions (like CRUD)
+    // GET info from API
+    func getCharacters(completion: @escaping ([Character]?) -> Void) {
+        // Make the URL
+        guard var baseURL = URL(string: baseURLString) else { return }
+        // let components = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        baseURL.appendPathComponent(characterString)
+        print("\(baseURL)")
+        // Get info from the URL
+        let dataTask = URLSession.shared.dataTask(with: baseURL) { (data, _, error) in
+            if let error = error {
+                print("There was an error with dataTask: \(error.localizedDescription)")
+                return
+            }
+            guard let data = data else { return }
+               // Turn that intom Rick and Morty Characters
+            let jsonDecoder = JSONDecoder()
+           
+            do {
+               let characters = try jsonDecoder.decode([Character].self, from: data)
+                self.characterList = characters
+            } catch {
+                print("There was an error with the jsonDecoder: \(error.localizedDescription)")
+                return
+            }
+        }.resume()
+
+    }// END OF FETCH
+    
+    //END OF FILE
+    //MARK: - Extenstions (separate extension for each feature, such as searching or alert controllers)
 }
