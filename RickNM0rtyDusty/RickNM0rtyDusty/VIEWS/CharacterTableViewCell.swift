d63e5e78b7aef12f54b650b5c6a85d496266aa35
Austin West <korruptedanomaly@gmail.com>
2019-07-07 13:20:38 -0600

    Character cell complete

---
+++ RickNM0rtyDusty/RickNM0rtyDusty/VIEWS/CharacterTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterTableViewCell.swift
+//  RickNM0rtyDusty
+//
+//  Created by Austin West on 7/7/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+import UIKit
+
+class CharacterTableViewCell: UITableViewCell {
+
+    // MARK: - OUTLETS
+    
+    @IBOutlet weak var characterNameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var originLabel: UILabel!
+    @IBOutlet weak var characterPictureImageView: UIImageView!
+    
+} // END OF CLASS
